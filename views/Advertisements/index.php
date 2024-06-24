6cbe1fc83f4350d81e9a2820443459397930da37
Máté Molnár <mmateka89@gmail.com>
2024-06-25 00:35:36 +0200

    fix Advertisement view

--- views/Advertisements/index.php
+++ views/Advertisements/index.php
@@ -1,6 +1,6 @@
 <h1>Advertisements</h1>
 <ul>
     <?php foreach ($advertisements as $advertisement): ?>
-        <li><?= htmlspecialchars($advertisement['username']) ?>: <?= htmlspecialchars($advertisement['title']) ?></li>
+        <li><?= htmlspecialchars($advertisement->username) ?>: <?= htmlspecialchars($advertisement->title) ?></li>
     <?php endforeach; ?>
 </ul>
