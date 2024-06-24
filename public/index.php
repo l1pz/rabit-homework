8be2063b6d6e99367c69f0b1361ad414727969fb
Máté Molnár <mmateka89@gmail.com>
2024-06-24 22:12:07 +0200

    add docs for autoloader

--- public/index.php
+++ public/index.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 define("ROOT_PATH", dirname(__DIR__));
 
 spl_autoload_register(function (string $class) {
+    // autoload classes from src directory
+    // directories should be names after namespaces
+    // \Framework\Router -> src/Framework/Router.php
+
+    // make all slashes forward slashes for both unix and windows compatibility
     $class = str_replace('\\', '/', $class);
+
     require ROOT_PATH . "/src/$class.php";
 });
 
