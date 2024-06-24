d5b4894377eb575da168181bd7060806bec0ab85
Máté Molnár <mmateka89@gmail.com>
2024-06-25 00:40:15 +0200

    add HomeController

---
+++ src/App/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Framework\Viewer;
+
+readonly class HomeController
+{
+    public function __construct(private Viewer $viewer)
+    {}
+    public function index(): void
+    {
+        echo $this->viewer->render('shared/header.php', ['title' => 'Users']);
+        echo $this->viewer->render('Home/index.php');
+        echo $this->viewer->render('shared/footer.php');
+    }
+}
