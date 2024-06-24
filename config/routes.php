2c46441ed0bcaed8e4fd7f105cc10e31efdc70bc
Máté Molnár <mmateka89@gmail.com>
2024-06-25 00:34:47 +0200

    fix typo in advertisement route

--- config/routes.php
+++ config/routes.php
@@ -5,6 +5,6 @@ use Framework\Router;
 $router = new Router;
 $router->add('/', 'HomeController', 'index');
 $router->add('/users', 'UserController', 'index');
-$router->add('/advertisements', 'AdvertisementsController', 'index');
+$router->add('/advertisements', 'AdvertisementController', 'index');
 
 return $router;
