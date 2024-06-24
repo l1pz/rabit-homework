c7bc6f670224c399289d5cc3b0c4ae6af27b42ff
Máté Molnár <mmateka89@gmail.com>
2024-06-25 00:26:08 +0200

    add view rendering in UserController

--- src/App/Controllers/UserController.php
+++ src/App/Controllers/UserController.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Services\UserService;
+use Framework\Viewer;
 
 class UserController
 {
-    public function __construct(private readonly UserService $userService)
+    public function __construct(private readonly UserService $userService, private readonly Viewer $viewer)
     {
     }
 
@@ -16,6 +17,8 @@ class UserController
     {
         $users = $this->userService->getAll();
 
-       var_dump($users);
+        echo $this->viewer->render('shared/header.php', ['title' => 'Users']);
+        echo $this->viewer->render('Users/index.php', ['users' => $users]);
+        echo $this->viewer->render('shared/footer.php');
     }
 }
