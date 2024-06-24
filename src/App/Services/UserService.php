308c36274e3b7dd190446150b6f4a3528eaeb506
Máté Molnár <mmateka89@gmail.com>
2024-06-25 00:06:02 +0200

    add UserService

---
+++ src/App/Services/UserService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\UserModel;
+use PDO;
+
+readonly class UserService
+{
+    public function __construct(private DatabaseService $database) {}
+
+    /**
+     * Gets all users
+     * @return UserModel[] An array of UserModels
+     */
+    public function getAll(): array
+    {
+        $pdo = $this->database->getConnection();
+        $stmt = $pdo->query('SELECT * FROM users');
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+}
