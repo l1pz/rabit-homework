f20898b33fc7a7a8923ff4ca3d44d4202fb5cc0d
Máté Molnár <mmateka89@gmail.com>
2024-06-24 22:34:20 +0200

    add Router class

---
+++ src/Framework/Router.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+class Router
+{
+    private array $routes = [];
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * @param string $path URL path
+     * @param string $controller Controller that this path corresponds to
+     * @param string $action Action that should be called on the controller
+     * @return void
+     */
+    public function add(string $path, string $controller, string $action): void
+    {
+        $path = rtrim($path, '/');
+        $this->routes[] = new Route($path, $controller, $action);
+    }
+
+}
