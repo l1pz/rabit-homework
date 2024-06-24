67d11eef3a1a23f155b5e282f04ea5c5d586dcdb
Máté Molnár <mmateka89@gmail.com>
2024-06-24 22:03:08 +0200

    add .env loader

---
+++ src/Framework/Dotenv.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Framework;
+
+class Dotenv
+{
+    /**
+     * Load variable from .env file at $path to $_ENV superglobal
+     * @param string $path Path to .env file
+     * @return void
+     */
+    public static function load(string $path): void
+    {
+        $lines = file($path, FILE_IGNORE_NEW_LINES);
+        foreach ($lines as $line) {
+            list($key, $value) = explode('=', $line);
+            $_ENV[$key] = $value;
+        }
+    }
+}
