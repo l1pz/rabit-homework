42bf321b249351162ffef0413549467aab8a2c71
Máté Molnár <mmateka89@gmail.com>
2024-06-25 01:18:05 +0200

    add styling to error pages

--- views/404.php
+++ views/404.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Page Not Found</title>
+
+    <link rel="stylesheet" href="/styles/reset.css">
+    <link rel="stylesheet" href="/styles/main.css">
 </head>
 <body>
 <img src="https://http.cat/images/404.jpg" alt="Picture of a cat with error code 500">
--- views/500.php
+++ views/500.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Error</title>
+
+    <link rel="stylesheet" href="/styles/reset.css">
+    <link rel="stylesheet" href="/styles/main.css">
 </head>
 <body>
 <img src="https://http.cat/images/500.jpg" alt="Picture of a cat with error code 500">
