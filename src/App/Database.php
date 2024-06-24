eebaf459e86b466f46a57006d7ef9f881ebdd849
Máté Molnár <mmateka89@gmail.com>
2024-06-24 22:47:51 +0200

    add Database class

---
+++ src/App/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use PDO;
+
+readonly class Database
+{
+    public function __construct(
+        public string $host,
+        public string $name,
+        public string $user,
+        public string $password
+    )
+    {
+    }
+
+    public function getConnection(): PDO
+    {
+        $dsn = "mysql:host=$this->host;dbname=$this->name;charset=utf8;port=3306";
+        return new PDO($dsn, $this->user, $this->password, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+
+    }
+}
