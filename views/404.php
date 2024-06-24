37d990091ec1aa277ff5d6a8c4f846fd58071b73
Máté Molnár <mmateka89@gmail.com>
2024-06-25 00:27:46 +0200

    add 404 page

---
+++ views/404.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page Not Found</title>
+</head>
+<body>
+<img src="https://http.cat/images/404.jpg" alt="Picture of a cat with error code 500">
+</body>
+</html>
