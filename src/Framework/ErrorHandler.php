c5febac36a4f0545735b54496f55e3d2d4a75e06
Máté Molnár <mmateka89@gmail.com>
2024-06-25 00:28:40 +0200

    add error 404 handling

--- src/Framework/ErrorHandler.php
+++ src/Framework/ErrorHandler.php
@@ -5,14 +5,20 @@ declare(strict_types=1);
 namespace Framework;
 
 use ErrorException;
+use Framework\Exceptions\PageNotFoundException;
 use Throwable;
 
 class ErrorHandler
 {
     public static function handleException(Throwable $exception)
     {
-        http_response_code(500);
-        $template = '500.php';
+        if ($exception instanceof PageNotFoundException) {
+            http_response_code(404);
+            $template = '404.php';
+        } else {
+            http_response_code(500);
+            $template = '500.php';
+        }
 
         if ($_ENV['SHOW_ERRORS']) {
             ini_set('display_errors', '1');
