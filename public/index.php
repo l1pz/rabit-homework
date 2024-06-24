86d8869827db981d788e9ede820b07850769866e
Máté Molnár <mmateka89@gmail.com>
2024-06-24 22:06:05 +0200

    add autoloader

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
 declare(strict_types=1);
 
-echo 'Hello World!';
+define("ROOT_PATH", dirname(__DIR__));
+
+spl_autoload_register(function (string $class) {
+    $class = str_replace('\\', '/', $class);
+    require ROOT_PATH . "/src/$class.php";
+});
